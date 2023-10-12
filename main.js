9837ae119a1ca8dc4687251b71017632d973e86f
Trixek24 <121521201+Trixek24@users.noreply.github.com>
2023-10-12 18:51:08 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+right_wrist_x = 0;
+right_wrist_y = 0;
+left_wrist_x = 0;
+left_wrist_y = 0;
 
 function preload()
 {
@@ -15,9 +19,31 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotposes);
 }
 
 function draw()
 {
     image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+
+}
+
+function gotposes(results)
+{
+    if(results.length > 0)
+    {
+        right_wrist_x = results[0].pose.rightWrist.x;
+        right_wrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+right_wrist_x + "rightWristY = "+ right_wrist_y);
+        left_wrist_x = results[0].pose.leftWrist.x;
+        left_wrist_y =  results[0].pose.leftWrist.y;
+        console.log("leftWrist X = "+left_wrist_x+" leftWrist Y = "+left_wrist_y);
+    }
 }
